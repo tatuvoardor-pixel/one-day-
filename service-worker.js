44781548b41d66df933606a0003531a207d83c6b
tatuvoardor-pixel <tatuvoardor@gmail.com>
2026-06-28 05:34:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,6 @@
-/* MAX.CORE Service Worker v1.1 */
-const CACHE_NAME = 'maxcore-v1.1';
-const ARQUIVOS = ['./','./index.html','./bio-bridge.js'];
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS)));
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((nomes) =>
-            Promise.all(nomes.map((n) => caches.delete(n)))
-        ).then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).then((resp) => {
-            const copia = resp.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copia));
-            return resp;
-        }).catch(() => caches.match(event.request))
-    );
-});
+/* MAX.CORE Service Worker v1.5 */
+const CACHE_NAME='maxcore-v1.5';
+const ARQUIVOS=['./','./index.html','./bio-bridge.js'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ARQUIVOS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(n=>Promise.all(n.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE_NAME).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
